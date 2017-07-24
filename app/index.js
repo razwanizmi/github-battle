821cdf69b1a8b144f2c26b0cca60062b2c39b28b
Razwan Rashidi <razwan.izmi@gmail.com>
2017-07-24 23:08:11 +0800

    Create popular component

--- app/index.js
+++ app/index.js
@@ -1,16 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var App = require('./components/App');
 require('./index.css');
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello Stranger!
-      </div>
-    )
-  }
-}
 
 ReactDOM.render(
   <App />,
